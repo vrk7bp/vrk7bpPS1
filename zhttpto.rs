8cfbce8b1db582805091de16dc163c2ff57512c0
vrk7bp <vrk7bp@virginia.edu>
2014-01-23 21:21:55 -0500

    Commit with 100% finished zhttpto.rs file with general comments

--- zhttpto.rs
+++ zhttpto.rs
@@ -1,14 +1,15 @@
+// Venkata-Gautam Kanumuru (vrk7bp)
 //
 // zhttpto.rs
 //
-// Starting code for PS1
+// Final code for PS1
 // Running on Rust 0.9
 //
 // Note that this code has serious security risks!  You should not run it 
 // on any system with access to sensitive files.
 // 
 // University of Virginia - cs4414 Spring 2014
-// Weilin Xu and David Evans
+// Originally coded by Weilin Xu and David Evans
 // Version 0.3
 
 #[feature(globs)];
@@ -63,6 +64,8 @@ fn main() {
 
             println(format!("Received request :\n{:s}", request_str));
             
+
+            //First check if there is no directories being accessed. If so then show the original "home screen".
             if (std::str::eq(&path, &testVal)) {
                 let response: ~str = 
                 ~"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
@@ -76,12 +79,33 @@ fn main() {
                  </body></html>\r\n";
                 stream.write(response.as_bytes());
             }
+
+            //Second, check to see if the last four characters in the path are .html. If so, then there are two possible scenarios...
             else if(std::str::eq(&isHTML, &HTMLTestVal)) {
+                let tempPath = path.clone();
                 let filePath = Path::new(path);
-                let mut msg_file = File::open(&filePath);
-                let msg_bytes: ~[u8] = msg_file.read_to_end();
-                stream.write(msg_bytes);               
+
+                //The path/document is valid and the particular file will be shown through the browser.
+                if(filePath.exists()) {
+                    let mut msg_file = File::open(&filePath);
+                    let msg_bytes: ~[u8] = msg_file.read_to_end();
+                    stream.write(msg_bytes); 
+                } 
+
+                //Otherwise the path/document is not valid and the 403 page will be brought up with the relevant path.
+                else {
+                    let response: ~str = 
+                    format!("HTTP/1.1 403 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                    <doctype !html><html><head><title>403 Forbidden</title>
+                    <body> <h1> Forbidden </h1>
+                    <p> You don't have permission to access /{:s} </p>
+                    <hr>
+                    </body></html>\r\n", tempPath);
+                    stream.write(response.as_bytes());   
+                };
             }
+
+            //If none of the above two situations were "met", then show the 403 page with the relevant path.
             else {
                 let response: ~str = 
                 format!("HTTP/1.1 403 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
@@ -92,6 +116,8 @@ fn main() {
                  </body></html>\r\n", path);
                 stream.write(response.as_bytes());
             };
+
+            //In the command prompt, print out "Connection terminates." as well as the new page visited number.
             println!("Connection terminates.");
             unsafe {
                 visitCount = visitCount + 1;
